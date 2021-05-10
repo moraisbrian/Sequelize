3693b8e86e613b06fff2d260c7850aad3dab76d8
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-05-10 16:08:37 -0300

    Removido pasta dist do projeto 'sequelize-typescript'

--- sequelize-typescript/dist/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./config/server");
-server_1.app.listen(3000, () => console.log('Server on'));
--- sequelize-typescript/dist/app/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const usuario_controller_1 = require("./controllers/usuario-controller");
-const router = express_1.Router();
-exports.router = router;
-router.post('/usuario', usuario_controller_1.usuarioController.adicionar);
